474b671b5a3bbd4b2e1812999de0c359b5ea1fea
Roger Braun <rogeriobflima@gmail.com>
2020-10-25 09:46:50 +0100

    First model-controller-view - department

--- WebSales/Controllers/HomeController.cs
+++ WebSales/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using WebSales.Models;
+using WebSales.Models.ViewModels;
 
 namespace WebSales.Controllers
 {
